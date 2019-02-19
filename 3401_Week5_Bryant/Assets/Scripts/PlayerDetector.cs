bca91ecd73ba154c090056f49443cfab5d9bc90e
MichaelBryantOU <46943678+MichaelBryantOU@users.noreply.github.com>
2019-02-19 14:19:28 -0500

    Project upload

---
+++ 3401_Week5_Bryant/Assets/Scripts/PlayerDetector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+
+
+    void FixedUpdate()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, fwd, 5))
+        {
+            print(gameObject.name);
+        }
+        else
+        {
+            print("Nothing ahead.");
+        }
+    }
+}
---
+++ 3401_Week5_Bryant/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    Vector3 targetPosition;
+    public float playerStand;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(cameraRay, out hit))
+            {
+                targetPosition = new Vector3(hit.point.x, playerStand, hit.point.z);
+                //transform.position = targetPosition;
+            }
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 0.05f);
+    }
+}
